b0cdf521f2d4c71cc999d72d3a820b5a93e37c23
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2021-11-15 14:13:25 +0100

    Separate font sizes for desktop and laptop

--- apply-config.sh
+++ apply-config.sh
@@ -3,14 +3,15 @@
 PWD=$(pwd)
 LN="ln -sf"
 CONFIG_DIR="$HOME/.config"
-# get conky version to use from system hostname
-CONKY_VERSION=$(hostname | sed 's/julian-//')
+# get device name
+DEVICE=$(hostname | sed 's/julian-//')
 
 # create symbolic links for config files and dirs
 $LN "$PWD/vimrc" "$HOME/.vimrc"
 $LN "$PWD/alacritty.yml" "$HOME/.alacritty.yml"
+$LN "$PWD/alacritty-config-$DEVICE.yml" "$HOME/.alacritty-config.yml"
 $LN "$PWD/spectrwm.conf" "$HOME/.spectrwm.conf"
-$LN "$PWD/conkyrc-$CONKY_VERSION" "$HOME/.conkyrc"
+$LN "$PWD/conkyrc-$DEVICE" "$HOME/.conkyrc"
 $LN "$PWD/ranger" "$CONFIG_DIR/"
 $LN "$PWD/zathurarc" "$CONFIG_DIR/zathura/zathurarc"
 
