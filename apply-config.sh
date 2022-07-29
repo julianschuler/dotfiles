644350e4c001100c8eab3716e62a2236e8eaddd7
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2022-07-29 19:56:12 +0200

    Reformat and add debug output

--- apply-config.sh
+++ apply-config.sh
@@ -1,62 +1,69 @@
 #!/bin/sh
 
-ln_cmd="ln -sf"
-config_dir="$HOME/.config"
-dot_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-device=$(hostnamectl hostname | cut -d- -f2)
+# exit on errors
+set -e
+
+# print usage information and exit
+print_usage() {
+    >&2 echo \
+"Usage: $(basename $0) [OPTIONS]
+Apply (parts of) the configuration to the system.
+
+Available options (at least one required):
+  -a    Apply all of the configuration, equivalent to -lpf
+  -l    Create symlinks for configuration files and directories
+  -p    Install packages listed in packages.txt using paru
+  -f    Setup fish and install fish plugins
+  -h    Show this help message"
+    exit 0
+}
+
+# output message to stderr
+print_debug() {
+    >&2 echo "$1"
+}
+
+# print error message and exit
+print_error() {
+	if [ "$#" -gt 0 ]; then
+		>&2 echo "$1"
+	fi
+	>&2 echo "Use \"$(basename $0) -h\" for further information."
+	exit 1
+}
 
 # flags to be set
 create_links=false
 install_packages=false
 setup_fish=false
 
-
-usage() {
-    >&2 echo \
-"Usage: $(basename $0) [OPTIONS]
-Available flags (at least one required):
-  -a  Apply all configuration, equivalent to -lpf
-  -l  Create symlinks for config files and directories
-  -p  Install packages listed in packages.txt using paru
-  -f  Setup fish and install fish plugins
-  -h  Show this help message"
-}
-
-if [[ $# -eq 0 ]]; then
-    usage
-    exit 1;
-fi
-
 # set flags according to input parameters
 while getopts "alfph" arg; do
     case $arg in
-        "a")
-            create_links=false
-            install_packages=false
-            setup_fish=false
-            ;;
-        "f")
-            setup_fish=true
-            ;;
-        "l")
-            create_links=true
-            ;;
-        "p")
-            install_packages=true
-            ;;
-        "h")
-            usage
-            exit 0;
-            ;;
-        "?")
-            usage
-            exit 1;
-            ;;
+        "a") create_links=false; install_packages=false; setup_fish=false ;;
+        "f") setup_fish=true ;;
+        "l") create_links=true ;;
+        "p") install_packages=true ;;
+        "h") print_usage; exit 0 ;;
+        "?") print_error; exit 1 ;;
     esac
 done
 
+# test if at least one flag was specified
+if [ "$OPTIND" -eq 1 ]; then
+    print_error "No options specified."
+fi
+shift $(( OPTIND - 1 ))
+
+# commands and directories
+ln_cmd="ln -sf"
+config_dir="$HOME/.config"
+dot_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+device=$(hostnamectl hostname | cut -d- -f2)
+
 # create symbolic links for config files and directories
-if [ "$CREATE_LINKS" = true ]; then
+if [ "$create_links" = true ]; then
+    print_debug "Creating symlinks for the configuration files and directories..."
     $ln_cmd "$dot_dir/aerc" "$config_dir/"
     $ln_cmd "$dot_dir/alacritty" "$config_dir/"
     $ln_cmd "$dot_dir/alacritty/alacritty-$device.yml" "$dot_dir/alacritty/alacritty.yml"
@@ -69,14 +76,21 @@ if [ "$CREATE_LINKS" = true ]; then
     $ln_cmd "$dot_dir/vimrc" "$HOME/.vimrc"
     $ln_cmd "$dot_dir/xinitrc" "$HOME/.xinitrc"
     $ln_cmd "$dot_dir/zathura" "$config_dir/"
+    print_debug ""
 fi
 
 # install packages using paru
 if [ "$install_packages" = true ]; then
+    print_debug "Installing packages from package.txt using paru..."
     cut "-d " -f1 "$dot_dir/packages.txt" | xargs paru -S
+    print_debug ""
 fi
 
 # setup fish and install fish plugins
 if [ "$setup_fish" = true ]; then
+    print_debug "Setting up fish..."
     fish "$dot_dir/fish/install.fish"
+    print_debug ""
 fi
+
+print_debug "Configuration applied sucessfully."
