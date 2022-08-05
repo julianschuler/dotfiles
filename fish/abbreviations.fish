2f31eaba54d050ef3b81172cf8ba5dad9b0bf1cb
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2022-08-05 20:33:02 +0200

    Update abbreviations and add completions

--- fish/abbreviations.fish
+++ fish/abbreviations.fish
@@ -3,30 +3,32 @@ abbr -a -U -- c z
 abbr -a -U -- d 'zathura --fork'
 abbr -a -U -- e vim
 abbr -a -U -- f fd
-abbr -a -U -- l exa
-abbr -a -U -- ll exa --long --git --header
+abbr -a -U -- l ls
 abbr -a -U -- m man
+abbr -a -U -- p python3
 abbr -a -U -- r ranger
 abbr -a -U -- s sudo
 abbr -a -U -- t bat
-abbr -a -U -- v vim
 abbr -a -U -- x exit
 
-# apt
-abbr -a -U -- aa 'sudo apt autoremove'
-abbr -a -U -- ai 'sudo apt install'
-abbr -a -U -- ar 'sudo apt remove'
-abbr -a -U -- as 'apt search'
-abbr -a -U -- au 'sudo apt update && sudo apt upgrade'
+# paru
+abbr -a -U -- a paru
+abbr -a -U -- ai 'paru -S'
+abbr -a -U -- ar 'paru -R'
+abbr -a -U -- aq 'paru -Q'
 
 # git
 abbr -a -U -- g git
 abbr -a -U -- ga 'git add'
 abbr -a -U -- gc 'git commit'
+abbr -a -U -- gcl 'git clone'
 abbr -a -U -- gco 'git checkout'
 abbr -a -U -- gf 'git diff'
 abbr -a -U -- gl 'git log'
 abbr -a -U -- gm 'git merge'
 abbr -a -U -- gr 'git restore --staged'
+abbr -a -U -- gR 'git restore'
 abbr -a -U -- gs 'git status'
+abbr -a -U -- gsh 'git stash'
 abbr -a -U -- gu 'git push'
+abbr -a -U -- gul 'git pull'
---
+++ fish/aliases.fish
@@ -0,0 +1,4 @@
+alias -s grubdate 'sudo grub-mkconfig -o /boot/grub/grub.cfg'
+alias -s ls exa
+alias -s la 'exa -a'
+alias -s ll 'exa -la --git'
