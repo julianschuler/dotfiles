3eac3558242e2511154fd4a3098ee1a12875ebf0
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2022-09-11 18:32:22 +0200

    Silence X during startup and show groups with ll

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,4 +1,4 @@
 alias -s grubdate 'sudo grub-mkconfig -o /boot/grub/grub.cfg'
 alias -s ls exa
 alias -s la 'exa -a'
-alias -s ll 'exa -la --git'
+alias -s ll 'exa -lag --git'
--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,2 @@
 # Start X at login
-status is-login && [ -z "$DISPLAY" -a "$XDG_VTNR" = 1 ]  && exec startx
+status is-login && [ -z "$DISPLAY" -a "$XDG_VTNR" = 1 ]  && exec startx &> /dev/null
