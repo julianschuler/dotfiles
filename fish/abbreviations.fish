6eacdbf03c63b4e9af95d0bb3d70a70507c8c358
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2023-01-11 11:39:58 +0100

    Update fish abbreviations

--- fish/abbreviations.fish
+++ fish/abbreviations.fish
@@ -1,37 +1,62 @@
 # general
+abbr -a -U -- b bat
 abbr -a -U -- c z
 abbr -a -U -- d pdf
 abbr -a -U -- e nvim
 abbr -a -U -- f fd
 abbr -a -U -- l ls
 abbr -a -U -- m man
-abbr -a -U -- p python3
+abbr -a -U -- n nvim
+abbr -a -U -- p python
 abbr -a -U -- r ranger
 abbr -a -U -- s sudo
 abbr -a -U -- se sudo rvim
-abbr -a -U -- t bat
+abbr -a -U -- t tldr
 abbr -a -U -- x exit
 
 # paru
 abbr -a -U -- a paru
-abbr -a -U -- as 'paru -S'
-abbr -a -U -- ar 'paru -Rs'
+abbr -a -U -- ag 'paru -Gp'
 abbr -a -U -- aq 'paru -Q'
+abbr -a -U -- aqe 'paru -Qe'
+abbr -a -U -- aqi 'paru -Qi'
+abbr -a -U -- ar 'paru -Rs'
+abbr -a -U -- as 'paru -S'
+abbr -a -U -- asi 'paru -Si'
+abbr -a -U -- asi 'paru -Ss'
+abbr -a -U -- asy 'paru -Sy'
 
 # git
 abbr -a -U -- g git
 abbr -a -U -- ga 'git add'
-abbr -a -U -- gb 'git rebase'
+abbr -a -U -- gap 'git add -p'
 abbr -a -U -- gc 'git commit'
+abbr -a -U -- gca 'git commit -a'
 abbr -a -U -- gcl 'git clone'
-abbr -a -U -- gco 'git checkout'
-abbr -a -U -- gf 'git diff'
+abbr -a -U -- gclr 'git clone --recursive'
+abbr -a -U -- gf 'git fetch'
+abbr -a -U -- gi 'git diff'
+abbr -a -U -- gis 'git diff --staged'
 abbr -a -U -- gl 'git log'
+abbr -a -U -- glp 'git log -p'
+abbr -a -U -- gls 'git log --stat'
 abbr -a -U -- gm 'git merge'
-abbr -a -U -- gr 'git restore --staged'
+abbr -a -U -- gr 'git restore -S'
+abbr -a -U -- grb 'git rebase'
+abbr -a -U -- grbc 'git rebase --continue'
+abbr -a -U -- grbi 'git rebase -i'
+abbr -a -U -- grm 'git remote'
+abbr -a -U -- grmv 'git remote -v'
 abbr -a -U -- gR 'git restore'
-abbr -a -U -- gs 'git status'
-abbr -a -U -- gsh 'git stash'
+abbr -a -U -- gs 'git status -s'
+abbr -a -U -- gsh 'git show'
+abbr -a -U -- gsl 'git status'
+abbr -a -U -- gsm 'git submodule'
+abbr -a -U -- gst 'git stash'
+abbr -a -U -- gstl 'git stash list'
+abbr -a -U -- gsto 'git stash pop'
+abbr -a -U -- gsw 'git switch'
+abbr -a -U -- gswc 'git switch -c'
 abbr -a -U -- gu 'git push'
+abbr -a -U -- guf 'git push --force-with-lease'
 abbr -a -U -- gul 'git pull'
-abbr -a -U -- gw 'git show'
