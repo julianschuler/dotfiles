413857a0439b5d5aceb4dbfc9596fc88b242eaa3
Julian Schuler <julianschuler@users.noreply.github.com>
2024-05-10 14:28:25 +0200

    Add new ip and git abbreviations

--- fish/abbreviations.fish
+++ fish/abbreviations.fish
@@ -5,7 +5,11 @@ abbr -a d pdf
 abbr -a e nvim
 abbr -a f fd
 abbr -a h helix
+abbr -a ir 'ip route'
+abbr -a ia 'ip -brief address'
+abbr -a il 'ip -brief link'
 abbr -a l ls
+abbr -a lb lsblk
 abbr -a m man
 abbr -a p python
 abbr -a r ranger
@@ -34,14 +38,19 @@ abbr -a g git
 abbr -a ga 'git add'
 abbr -a gap 'git add -p'
 abbr -a gb 'git branch'
+abbr -a gba 'git branch -a'
+abbr -a gbd 'git branch -d'
+abbr -a gbm 'git branch -m'
 abbr -a gc 'git commit'
 abbr -a gca 'git commit -a'
+abbr -a gch 'git cherrypick'
 abbr -a gcl 'git clone'
 abbr -a gclr 'git clone --recursive'
-abbr -a gf 'git fetch'
+abbr -a gf 'git fetch --prune'
 abbr -a gi 'git diff'
 abbr -a gis 'git diff --staged'
 abbr -a gl 'git log'
+abbr -a glo 'git log --oneline --decorate --all --graph'
 abbr -a glp 'git log -p'
 abbr -a gls 'git log --stat'
 abbr -a gm 'git merge'
@@ -63,5 +72,6 @@ abbr -a gsto 'git stash pop'
 abbr -a gsw 'git switch'
 abbr -a gswc 'git switch -c'
 abbr -a gu 'git push'
+abbr -a gud 'git push -d'
 abbr -a guf 'git push --force-with-lease'
 abbr -a gul 'git pull'
