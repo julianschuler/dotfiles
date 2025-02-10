643249f64abd489e0f2a55bab1d96a23a3acd455
Julian Schuler <julianschuler@users.noreply.github.com>
2025-02-10 16:46:14 +0100

    Start niri at login

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
-# Start Hyprland at login
-status is-login && [ -z "$DISPLAY" -a "$XDG_VTNR" = 1 ]  && exec ssh-agent Hyprland &> /dev/null
+# Start niri at login
+status is-login && [ -z "$DISPLAY" -a "$XDG_VTNR" = 1 ] && exec ssh-agent niri-session
 
 set -l script_dir (realpath (dirname (status -f)))
 source "$script_dir/abbreviations.fish"
