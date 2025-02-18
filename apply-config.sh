dc503cb5520ee8404f4d91de4067424e79287511
Julian Schuler <julianschuler@users.noreply.github.com>
2025-02-18 17:05:04 +0100

    Use both WLR and GTK desktop portal within niri

--- apply-config.sh
+++ apply-config.sh
@@ -91,6 +91,7 @@ if [ "$create_links" = true ]; then
     $ln_cmd "$dot_dir/vimrc" "$HOME/.vimrc"
     $ln_cmd "$dot_dir/waybar" "$config_dir/"
     $ln_cmd "$dot_dir/wlogout" "$config_dir/"
+    $ln_cmd "$dot_dir/xdg-desktop-portal" "$config_dir/"
     $ln_cmd "$dot_dir/yofi" "$config_dir/"
     $ln_cmd "$dot_dir/zathura" "$config_dir/"
     print_debug ""
