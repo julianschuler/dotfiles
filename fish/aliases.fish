5f6f56c235c68e511d65a157674f3c58dae7948b
Julian Schuler <julianschuler@users.noreply.github.com>
2025-10-31 17:35:52 +0100

    Replace pdf alias with a script to suppress noise

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,7 +1,6 @@
 alias -s ls exa
 alias -s la 'exa -a'
 alias -s ll 'exa -lag --git'
-alias -s pdf 'zathura --fork'
 alias -s tree 'exa -T'
 alias -s mnt 'udisksctl mount -b'
 alias -s umnt 'udisksctl unmount -b'
